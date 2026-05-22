2b3de8eab275f183811fd70995b198c06303f53d
Fue Vang <Fseth023@gmail.com>
2026-05-22 09:29:17 -0500

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-ssh home "cd ~/Projects/knect && git pull && ~/.local/bin/docker-compose --env-file backend/.env -f backend/docker-compose.prod.yml up --build -d"
+ssh -o ServerAliveInterval=30 -o ServerAliveCountMax=100 home "cd ~/Projects/knect && git pull && ~/.local/bin/docker-compose --env-file backend/.env -f backend/docker-compose.prod.yml up --build -d"

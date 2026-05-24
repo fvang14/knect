f54293568af2bb1043e960614a4e402453a61023
Fue Vang <Fseth023@gmail.com>
2026-05-23 20:26:40 -0500

    feat(mobile): scaffold Expo contractor app

---
+++ mobile/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+};
